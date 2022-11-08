48d9ce8af3e147a71a51068d1812287652911445
İlker <lker@DESKTOP-DH0LVGV>
2022-11-08 22:30:39 +0300

    Add project files.

---
+++ 12.10.2022/Program.cs
@@ -0,0 +1,50 @@
+﻿int a = 20;
+long l = 23233;
+short s = 232;
+double d = 23.4;
+decimal dc = 34.5m;
+float f = 234.4f;
+byte byt = 234;
+
+bool bl = true;
+char c = 'a';
+
+string str = "İlker";
+object obj = 2324;
+
+/*Console.WriteLine(int.MaxValue);
+Console.WriteLine(long.MaxValue);*/
+
+int ogr_id = 23;
+string ogr_adsyd = "İlker Şatur";
+byte ogr_yas = 22;
+bool gender = true;
+double not_ort = 65.7;
+
+Console.WriteLine("öğrenci");
+    Console.Write("id = ");
+    int ogrenci_id = Convert.ToInt16(Console.ReadLine());
+    Console.Write("Ad Soyad = ");
+    string ogrenci_ad_soyad = (Console.ReadLine());
+    Console.Write("Yaş = ");
+    byte ogrenci_yas = Convert.ToByte(Console.ReadLine());
+    Console.Write("Doğum tarihi gün ay yıl = ");
+    //doğum tarihi sene ekleme
+    //console.writeline(dogumtarihi.addyears(10));
+    DateTime dogumtarihi = DateTime.Parse(Console.ReadLine());
+    Console.Write("Cinsiyet K ya da E  ");
+    char cinsiyet = Convert.ToChar(Console.ReadLine());
+    if (cinsiyet == 'K') ;
+    {
+        Console.WriteLine("KADIN"); }
+    if (cinsiyet == 'E') ;
+    {
+        Console.WriteLine("ERKEK");
+    }
+    Console.WriteLine("not ortolaması ");
+    Console.WriteLine("vize ");
+    int vize = Convert.ToInt16(Console.ReadLine());
+    Console.WriteLine("final ");
+    int final = Convert.ToInt16(Console.ReadLine());
+    int ortalama = (vize + final) / 2;
+    Console.WriteLine("ortalama " + ortalama);
---
+++ maas_zam/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("personel");
+Console.Write("id = ");
+int personel_id = Convert.ToInt16(Console.ReadLine());
+Console.Write("Ad Soyad = ");
+string personel_ad_soyad = (Console.ReadLine());
+Console.Write("Yaş = ");
+byte yas = Convert.ToByte(Console.ReadLine());
+Console.Write("Maaş = ");
+double maas = Convert.ToDouble(Console.ReadLine());
+Console.Write("zam oranı %");
+double zam = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("zam oranı %");
+
+double yeni_maas = maas + ((maas * zam) / 100);
+Console.WriteLine(personel_ad_soyad + " Zammı maaş " + yeni_maas + " TL");
+
+static double Zamyap(double maas, double zam)      //metod yapma
+{
+    return maas + maas * zam / 100;
+}
+double zamlımaas = Zamyap(maas, 20);
+
+
+//tip bilemiyorsak var kullanırız 
+var metin = "ilker";
+var sayi = 123;
+
+
+string nesne = "Telefon";
+string marka = "Samsung";
+string renk = "Mavi";
+DateTime cikis_yili = DateTime.Parse("12.01.2022");
+double android_surumu = 12.1;
+byte ram = 8;
+bool parmak_izi = true;
+int batarya_Mah = 4300;
+decimal fiyat = 8200;
+
+Console.WriteLine("yaş gir");
+string yas2 = Console.ReadLine();
+byte yeniyas = byte.Parse(yas2);
+Console.WriteLine(yeniyas);
+
+x:
+try { 
+Console.WriteLine("yaş gir");
+string age = Console.ReadLine();
+byte new_age = byte.Parse(age); //convert.to ile .parse aynı işlemi yapıyor
+Console.WriteLine(new_age);}
+catch
+{
+    Console.WriteLine("!!!!!sayi gir!!!!!");
+}
+goto x;
